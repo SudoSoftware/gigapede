489eedee0a508e8ff89ab6d3cfe9a6690992d274
Jesse Victors <jvictors@jessevictors.com>
2013-03-14 12:19:56 -0600

    scorpions (#13) now spawn

--- gigapede/gigapede/CentipedeGame.cs
+++ gigapede/gigapede/CentipedeGame.cs
@@ -21,6 +21,7 @@ namespace gigapede
 
 		Texture2D background;
 		MyRandom prng = new MyRandom();
+		PointF centipedeSpawnLoc = new PointF(0, 0);
 		UserInput userInput;
 		World world;
 
@@ -77,7 +78,7 @@ namespace gigapede
 		public void AddWorldContent()
 		{
 			AddMushrooms();
-			world.AddItem(new Centipede(new PointF(0, 0)));
+			//adding of Centipedes and Scorpions are handled in Update function
 			world.AddItem(new Shooter(new PointF(
 				(world.getBounds().Width + GameParameters.DEFAULT_ITEM_WIDTH) / 2,
 				world.getBounds().Height - GameParameters.DEFAULT_ITEM_HEIGHT
@@ -88,17 +89,17 @@ namespace gigapede
 
 		private void AddMushrooms()
 		{
-			int startY = GameParameters.EMPTY_HEADER_ROWS * GameParameters.DEFAULT_ITEM_HEIGHT;
+			float startY = GameParameters.EMPTY_HEADER_ROWS * GameParameters.DEFAULT_ITEM_HEIGHT;
 			float endY = world.getBounds().Height - GameParameters.EMPTY_FOOTER_ROWS * GameParameters.DEFAULT_ITEM_HEIGHT;
 			float endX = world.getBounds().Width - GameParameters.DEFAULT_ITEM_WIDTH;
 
-			for (int x = 0; x < endX; x += GameParameters.DEFAULT_ITEM_WIDTH)
-				for (int y = startY; y < endY; y += GameParameters.DEFAULT_ITEM_HEIGHT)
-					if (prng.nextRange(0, 10) <= 1) //if there isn't a mushroom directly northwest, 10% chance of adding
+			for (float x = 0; x < endX; x += GameParameters.DEFAULT_ITEM_WIDTH)
+				for (float y = startY; y < endY; y += GameParameters.DEFAULT_ITEM_HEIGHT)
+					if (prng.nextRange(0, 10) <= 1) // 10% chance of adding
 						world.AddItem(new Mushroom(new PointF(x, y)));
 		}
 
-		int centipedeCount = 10;
+
 
 		protected override void Update(GameTime gameTime)
 		{
@@ -107,14 +108,37 @@ namespace gigapede
 			if (userInput.justPressed(UserInput.InputType.ESCAPE))
 				this.Exit();
 
+			HandleCentipedeSpawning();
+			HandleScorpionSpawning();
 			world.Update(gameTime, userInput);
-			if (centipedeCount > 0 && !world.TypeAt(new PointF(0, 0), 1f, typeof(Centipede)))
-			{
-				world.AddItem(new Centipede(new PointF(0, 0)));
-				centipedeCount--;
-			}
 
 			base.Update(gameTime);
+		}
+
+
+
+		int centipedeCount = 10;
+		private void HandleCentipedeSpawning()
+		{
+			if (centipedeCount > 0 && !world.TypeAt(centipedeSpawnLoc, 1f, typeof(Centipede)))
+			{
+				world.AddItem(new Centipede(centipedeSpawnLoc));
+				centipedeCount--;
+			}
+		}
+
+
+
+		private void HandleScorpionSpawning()
+		{
+			if (prng.nextRange(0, 1000) <= 1) // 0.2% of spawning
+			{
+				float range = prng.nextRange(GameParameters.EMPTY_HEADER_ROWS, GameParameters.GRID_SIZE - GameParameters.EMPTY_FOOTER_ROWS);
+				float x = 0; //temporary
+				float y = (float)Math.Round(range) * GameParameters.DEFAULT_ITEM_HEIGHT;
+
+				world.AddItem(new Scorpion(new PointF(x, y)));
+			}
 		}
 
 
