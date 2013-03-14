bd21737b293206e1d44440f690be052246b263cf
Jesse Victors <jvictors@jessevictors.com>
2013-03-14 14:33:25 -0600

    added Flea class (#11)

---
+++ gigapede/gigapede/GameItems/Flea.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using System.Drawing;
+using gigapede.Resources;
+
+namespace gigapede.GameItems
+{
+	class Flea : JumpingGameItem
+	{
+		public static Texture2D texture;
+
+
+		public Flea(PointF location) :
+			base(location)
+		{ }
+
+
+
+		protected override void Jump(InfoForItem info)
+		{
+
+		}
+
+
+		
+		public override float GetSpeed()
+		{
+			return GameParameters.FLEA_SPEED;
+		}
+
+
+
+		public override Texture2D GetTexture()
+		{
+			return texture;
+		}
+	}
+}
