bfbe074e77ac345ce925f9aa9831654444d4c95c
Jesse Victors <jvictors@jessevictors.com>
2013-03-13 23:45:40 -0600

    more work on #10: initial code for centipede body

--- gigapede/gigapede/GameItems/Centipede.cs
+++ gigapede/gigapede/GameItems/Centipede.cs
@@ -12,7 +12,7 @@ namespace gigapede.GameItems
 	class Centipede : DamageableGameItem
 	{
 		public static Texture2D texture;
-		private List<RectangleF> body = new List<RectangleF>();
+		private LinkedList<RectangleF> body = new LinkedList<RectangleF>();
 		private bool movingRight = true;
 		private float movementTillJump;
 
@@ -20,8 +20,14 @@ namespace gigapede.GameItems
 		public Centipede(PointF location) :
 			base(location)
 		{
-			//preciseOrigin = boundingBox.Location;
 			ResetJumpWait();
+
+			RectangleF currentSegment = boundingBox;
+			for (int j = 0; j < 10; j++)
+			{
+				body.AddLast(currentSegment);
+				currentSegment = new RectangleF(new PointF(currentSegment.X - boundingBox.Width, currentSegment.Y), currentSegment.Size);
+			}
 		}
 
 
@@ -31,7 +37,7 @@ namespace gigapede.GameItems
 			float theta = info.gameTime.ElapsedGameTime.Milliseconds * GameParameters.CENTIPEDE_SPEED;
 			movementTillJump -= theta;
 
-			if (movementTillJump < 0)
+			if (movementTillJump <= 0)
 			{
 				Jump(info);
 				ResetJumpWait();
@@ -44,13 +50,14 @@ namespace gigapede.GameItems
 
 		private void Jump(InfoForItem info)
 		{
-			PointF nextLoc = new PointF(boundingBox.Location.X, boundingBox.Location.Y);
+			PointF headLocation = body.First.Value.Location;
+			
+			PointF nextLoc = headLocation;
 			Move(ref nextLoc);
-
 
 			if (!info.world.IsLegalLocation(new RectangleF(nextLoc, boundingBox.Size)) || info.world.ItemAt(nextLoc, 1f) != null)
 			{
-				nextLoc.X = boundingBox.Location.X;
+				nextLoc.X = headLocation.X;
 				nextLoc.Y += boundingBox.Height;
 				movingRight = !movingRight;
 
@@ -59,6 +66,9 @@ namespace gigapede.GameItems
 			}
 
 			boundingBox.Location = nextLoc;
+
+			body.RemoveLast();
+			body.AddFirst(new RectangleF(nextLoc, boundingBox.Size));
 		}
 
 
@@ -77,68 +87,6 @@ namespace gigapede.GameItems
 		{
 			movementTillJump = boundingBox.Width;
 		}
-
-			/*
-			PointF nextLoc = new PointF(preciseOrigin.X, preciseOrigin.Y);
-			
-			if (!info.world.IsLegalLocation(new RectangleF(nextLoc, boundingBox.Size)) || IsContactingMushroom(info))
-			{
-				nextLoc.Y += boundingBox.Height;
-				movingRight = !movingRight;
-
-				//GameItem obj = info.world.ItemAt(nextLoc, 1f);
-				//if (obj != null && obj.GetType() == typeof(Mushroom))
-				if (info.world.GetContacts(
-				{
-					if (movingRight)
-						nextLoc.X += boundingBox.Width + 3;
-					else
-						nextLoc.X -= boundingBox.Width + 3;
-				}
-			}
-
-			float theta = info.gameTime.ElapsedGameTime.Milliseconds * GameParameters.CENTIPEDE_SPEED;
-			if (movingRight)
-				nextLoc.X += theta;
-			else
-				nextLoc.X -= theta;
-
-			preciseOrigin = nextLoc;
-			boundingBox.Location = preciseOrigin; //temporary, the Centipede should eventually stutter
-			
-			return base.Update(info);
-		}
-
-
-
-		private bool IsContactingMushroom(InfoForItem info)
-		{
-			foreach (GameItem item in info.contacts)
-				if (item.GetType() == typeof(Mushroom))
-					return true;
-			return false;
-		}
-
-
-
-		private bool BounceOffWalls(InfoForItem info)
-		{
-			return !info.world.IsLegalLocation(new RectangleF(preciseOrigin, boundingBox.Size));
-		}
-
-
-
-		private void UpdateLocation(InfoForItem info)
-		{
-			float theta = info.gameTime.ElapsedGameTime.Milliseconds * GameParameters.CENTIPEDE_SPEED;
-			if (movingRight)
-				preciseOrigin.X += theta;
-			else
-				preciseOrigin.X -= theta;
-
-			boundingBox.X = preciseOrigin.X; //temporary, the Centipede should eventually stutter
-			boundingBox.Y = preciseOrigin.Y;
-		}*/
 
 
 
